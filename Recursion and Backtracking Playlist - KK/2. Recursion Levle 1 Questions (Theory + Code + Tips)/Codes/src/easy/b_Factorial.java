09e5f494862365377fb41f068348a0cc947c06ca
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-02 16:18:32 +0600

    b_Factorial : Calculate factorial of a given number using recursion

---
+++ Recursion and Backtracking Playlist - KK/2. Recursion Levle 1 Questions (Theory + Code + Tips)/Codes/src/easy/b_Factorial.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class b_Factorial {
+    public static void main(String[] args) {
+
+        int ans = fact(5);
+
+        System.out.println(ans);
+    }
+
+    static int fact(int n){
+
+        if( n <= 1){
+            return 1;
+        }
+
+        return n * fact(n-1);
+    }
+}
