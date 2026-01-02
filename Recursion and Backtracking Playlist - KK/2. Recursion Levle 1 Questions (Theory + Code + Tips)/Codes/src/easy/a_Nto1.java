323562ab95de214d6c3188914a93b6834109246a
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-02 16:10:33 +0600

    a_Nto1 : print N to 1 or 1 to N numbers using recursion

---
+++ Recursion and Backtracking Playlist - KK/2. Recursion Levle 1 Questions (Theory + Code + Tips)/Codes/src/easy/a_Nto1.java
@@ -0,0 +1,38 @@
+package easy;
+
+public class a_Nto1 {
+    public static void main(String[] args) {
+        fun(5);
+        System.out.println();
+        funRev(5);
+        System.out.println();
+        funBoth(5);
+
+    }
+
+    static void fun(int n){
+        if ( n == 0){
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+
+    static void funRev(int n){
+        if(n == 0){
+            return;
+        }
+        funRev(n-1);
+        System.out.println(n);
+    }
+
+    static void funBoth(int n){
+        if( n == 0){
+            return;
+        }
+
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
+}
