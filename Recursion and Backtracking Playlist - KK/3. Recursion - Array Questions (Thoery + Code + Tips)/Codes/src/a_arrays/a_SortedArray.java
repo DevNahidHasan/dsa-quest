082af3361e9fbfa64d8d3cba97ac2dba3954c819
DevNahidHasan <smnahid.hsn@gmail.com>
2026-01-04 16:24:23 +0600

    Practiced a_SortedArray : Identify if an array is sorted or not.

---
+++ Recursion and Backtracking Playlist - KK/3. Recursion - Array Questions (Thoery + Code + Tips)/Codes/src/a_arrays/a_SortedArray.java
@@ -0,0 +1,17 @@
+package a_arrays;
+
+public class a_SortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 16, 8};
+        System.out.println(isSorted(arr,0));
+    }
+
+    static boolean isSorted(int[] arr, int index){
+        //base condition
+        if(index == arr.length -1){
+            return true;
+        }
+
+        return arr[index] < arr[index+1] && isSorted(arr, index + 1);
+    }
+}
