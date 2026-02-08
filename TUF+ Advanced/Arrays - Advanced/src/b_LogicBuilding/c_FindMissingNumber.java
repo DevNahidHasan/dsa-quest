314fe868378b09f8e5a195f491b1158f18326ab4
DevNahidHasan <smnahid.hsn@gmail.com>
2026-02-09 05:45:19 +0600

    Optimal II - XOR approach of problem -Find Missing Number

--- TUF+ Advanced/Arrays - Advanced/src/b_LogicBuilding/c_FindMissingNumber.java
+++ TUF+ Advanced/Arrays - Advanced/src/b_LogicBuilding/c_FindMissingNumber.java
@@ -3,7 +3,7 @@ package b_LogicBuilding;
 public class c_FindMissingNumber {
     public static void main(String[] args) {
         int[] nums = {0, 2, 3, 1, 4};
-        System.out.println(missingNumber(nums));
+        System.out.println(missingNumber2(nums));
     }
     // Optimal - I
     public static int missingNumber(int[] nums) {
@@ -18,5 +18,19 @@ public class c_FindMissingNumber {
         }
 
         return expectedTotal - givenTotal;
+    }
+
+    // Optimal II - XOR
+    public static int missingNumber2(int[] nums) {
+        int xor1 = 0, xor2 = 0;
+
+        // Calculate XOR of all array elements
+        for (int i = 0; i < nums.length; i++) {
+            xor1 = xor1 ^ (i + 1); // XOR up to [1...N]
+            xor2 = xor2 ^ nums[i]; // XOR of array elements
+        }
+
+        // XOR of xor1 and xor2 gives missing number
+        return (xor1 ^ xor2);
     }
 }
