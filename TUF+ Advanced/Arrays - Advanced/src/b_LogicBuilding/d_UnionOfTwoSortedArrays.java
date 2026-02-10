daf15ca70de556a7cba22fbf93cbbade9ca5ddfd
DevNahidHasan <smnahid.hsn@gmail.com>
2026-02-10 15:25:45 +0600

    Added Optimal Solution of problem - Union of Two Sorted Arrays

--- TUF+ Advanced/Arrays - Advanced/src/b_LogicBuilding/d_UnionOfTwoSortedArrays.java
+++ TUF+ Advanced/Arrays - Advanced/src/b_LogicBuilding/d_UnionOfTwoSortedArrays.java
@@ -1,8 +1,6 @@
 package b_LogicBuilding;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class d_UnionOfTwoSortedArrays {
     public static void main(String[] args) {
@@ -33,5 +31,53 @@ public class d_UnionOfTwoSortedArrays {
         }
 
         return union;
+    }
+
+    //Optimal Approach
+    public static int[] unionArray2(int[] nums1, int[] nums2) {
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+        int n = nums1.length;
+        int m = nums2.length;
+
+        while ( i < n && j < m){
+            if (nums1[i] <= nums2[j]) {
+                if (list.isEmpty() || list.get(list.size() - 1) != nums1[i]) {
+                    list.add(nums1[i]);
+                }
+                i++;
+            }
+            else {
+                if (list.isEmpty() || list.get(list.size() - 1) != nums2[j]) {
+                    list.add(nums2[j]);
+                }
+                j
+                        ++;
+            }
+        }
+
+        // Add remaining elements of nums1, if any
+        while (i < n) {
+            if (list.isEmpty() || list.get(list.size() - 1) != nums1[i]) {
+                list.add(nums1[i]);
+            }
+            i++;
+        }
+
+        // Add remaining elements of nums2, if any
+        while (j < m) {
+            if (list.isEmpty() || list.get(list.size() - 1) != nums2[j]) {
+                list.add(nums2[j]);
+            }
+            j++;
+        }
+
+        // Convert List<Integer> to int[]
+        int[] Union = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            Union[k] = list.get(k);
+        }
+
+        return Union;
     }
 }
