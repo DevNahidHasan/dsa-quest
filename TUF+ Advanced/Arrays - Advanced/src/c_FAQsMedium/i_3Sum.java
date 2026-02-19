7a5c6f142601129a725e944811a53df94335c871
DevNahidHasan <smnahid.hsn@gmail.com>
2026-02-19 23:01:51 +0600

    '3 Sum'- better solution

--- TUF+ Advanced/Arrays - Advanced/src/c_FAQsMedium/i_3Sum.java
+++ TUF+ Advanced/Arrays - Advanced/src/c_FAQsMedium/i_3Sum.java
@@ -19,7 +19,9 @@ public class i_3Sum {
         System.out.println();
     }
 /*
-Complexity Analyis
+Brute Force-
+
+Complexity Analysis-
 Time Complexity: O(N3 x log(no. of unique triplets)), where N is size of the array. Using 3 nested loops & inserting triplets into the set takes O(log(no. of unique triplets)) time complexity. But we are not considering the time complexity of sorting as we are just sorting 3 elements every time.
 
 Space Complexity: O(2 x no. of the unique triplets) for using a set data structure and a list to store the triplets.
@@ -47,6 +49,59 @@ Space Complexity: O(2 x no. of the unique triplets) for using a set data structu
                         tripletSet.add(temp);
                     }
                 }
+            }
+        }
+
+        // Convert set to list of lists (unique triplets)
+        List<List<Integer>> ans = new ArrayList<>(tripletSet);
+
+        //Return the ans
+        return ans;
+    }
+/*
+Better Approach -
+
+Complexity Analysis-
+
+Time Complexity: O(N2 x log(no. of unique triplets)), where N is size of the array.
+Inserting triplets into the set takes O(log(no. of unique triplets)) time complexity. However, we are not considering the time complexity of sorting, as we are only sorting 3 elements each time.
+Note: For Java (HashSet), insertion operation takes O(1) time. Thus, the overall time complexity for Java code will be O(N2)
+
+Space Complexity: O(2 x no. of the unique triplets) + O(N) for using a set data structure and a list to store the triplets and extra O(N) for storing the array elements in another set.
+ */
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        // Set to store unique triplets
+        Set<List<Integer>> tripletSet = new HashSet<>();
+
+        int n = nums.length;
+
+        // Check all possible triplets
+        for (int i = 0; i < n; i++) {
+            // Set to store elements seen so far in the loop
+            Set<Integer> hashset = new HashSet<>();
+
+            for (int j = i + 1; j < n; j++) {
+                // Calculate the 3rd element needed to reach 0
+                int third =  - (nums[i] + nums[j]);
+
+                /* Find if third element exists in
+                hashset (complements seen so far)*/
+                if (hashset.contains(third)) {
+                    // Found a triplet that sums up to target
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(third);
+
+                    /* Sort the triplet to ensure
+                    uniqueness when storing in set*/
+                    Collections.sort(temp);
+                    tripletSet.add(temp);
+                }
+
+                /* Insert the current element
+                into hashset for future checks*/
+                hashset.add(nums[j]);
             }
         }
 
