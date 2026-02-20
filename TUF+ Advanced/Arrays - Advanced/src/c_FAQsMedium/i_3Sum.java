1271fdc13f745218526b98d9904d911622b16621
DevNahidHasan <smnahid.hsn@gmail.com>
2026-02-20 22:45:49 +0600

    Added '3 Sum'- optimal solution

--- TUF+ Advanced/Arrays - Advanced/src/c_FAQsMedium/i_3Sum.java
+++ TUF+ Advanced/Arrays - Advanced/src/c_FAQsMedium/i_3Sum.java
@@ -111,4 +111,61 @@ Space Complexity: O(2 x no. of the unique triplets) + O(N) for using a set data 
         //Return the ans
         return ans;
     }
+    /*
+Optimal Approach -
+
+Complexity Analysis-
+
+Time Complexity: O(NlogN)+O(N2), where N is size of the array. As the pointer i, is running for approximately N times. And both the pointers j and k combined can run for approximately N times including the operation of skipping duplicates. So the total time complexity will be O(N2).
+
+Space Complexity: O(1), no extra space is used.
+
+
+     */
+
+    public static List<List<Integer>> threeSum3(int[] nums) {
+
+        // List to store the triplets that sum up to target
+        List<List<Integer>> ans = new ArrayList<>();
+
+        int n = nums.length;
+
+        // Sort the input array nums
+        Arrays.sort(nums);
+
+        // Iterate through the array to find triplets
+        for (int i = 0; i < n; i++) {
+            // Skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            // Two pointers approach
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    // Found a triplet that sums up to target
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    ans.add(temp);
+
+                    // Skip duplicates
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
 }
